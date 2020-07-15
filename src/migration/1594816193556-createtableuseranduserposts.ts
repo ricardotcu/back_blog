b82cd757f1d4dae8b36ee4062ec5635e70198172
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-15 10:23:44 -0300

    ajuste37

--- src/migration/1594816193556-createtableuseranduserposts.ts
+++ src/migration/1594816193556-createtableuseranduserposts.ts
@@ -15,8 +15,8 @@ export class createtableuseranduserposts1594816193556 implements MigrationInterf
         await queryRunner.query(
             `CREATE TABLE "user_posts" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-                "id_user" uuid NOT NULL UNIQUE,
-                "id_posts" uuid NOT NULL UNIQUE,
+                "id_user" uuid NOT NULL,
+                "id_posts" uuid NOT NULL,
                 CONSTRAINT "PK_3ef0759852caaefb9bac7cf913e" PRIMARY KEY ("id_user", "id_posts"))`
         );
         await queryRunner.query(
@@ -24,12 +24,6 @@ export class createtableuseranduserposts1594816193556 implements MigrationInterf
         );
         await queryRunner.query(
           `CREATE INDEX "IDX_9a616bdfff1c46bddeb8ad78db" ON "user_posts" ("id_posts") `,
-        );
-        await queryRunner.query(
-          `ALTER TABLE "user_posts" ADD CONSTRAINT "FK_ccb4ae4609bfbf4d022560a3f8c" FOREIGN KEY ("id_user") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-        );
-        await queryRunner.query(
-          `ALTER TABLE "user_posts" ADD CONSTRAINT "FK_9a616bdfff1c46bddeb8ad78dbf" FOREIGN KEY ("id_posts") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
         );
     }
 
